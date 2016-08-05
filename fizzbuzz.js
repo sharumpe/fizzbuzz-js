cd940929908dc05c0d28c3826b3b47e8b43bf6bd
Tim McLaughlin <Tim.McLaughlin@wwu.edu>
2016-08-05 11:44:43 -0700

    Conditions are now able to be specified.

--- fizzbuzz.js
+++ fizzbuzz.js
@@ -5,30 +5,33 @@ var config = {
       low: 1,
       high: 100
   },
-  fizz: {
-    cond: function (v) { return i%3==0 },
-    label: 'Fizz'
-  },
-  buzz: {
-    cond: function (v) { return i%5==0 },
-    label: 'Buzz'
-  },
-  fizzbuzz: {
-    cond: function (v) { return i%3==0 && i%5==0 },
-    label: 'FizzBuzz'
-  }
+  conditions: [
+    {
+      cond: function (v) { return i%3==0 },
+      label: 'Fizz'
+    },
+    {
+      cond: function (v) { return i%5==0 },
+      label: 'Buzz'
+    },
+    {
+      cond: function (v) { return i%8==0 },
+      label: 'Blip'
+    },
+    {
+      cond: function (v) { return i%3==0 && i%5==0 },
+      label: 'FizzBuzz'
+    }
+  ]
 };
 
+// Loop from low bound to high bound, and evaluate the int
 for (var i = config.bounds.low; i <= config.bounds.high; i++) {
   var result = i;
-  console.log(
-      "%s",
-      config.fizzbuzz.cond(i) 
-      ? config.fizzbuzz.label
-      : config.fizz.cond(i)
-        ? config.fizz.label
-        : config.buzz.cond(i)
-          ? config.buzz.label
-          : i
-  );
+  // Loop through the conditions, modify the 'result' if needed
+  config.conditions.forEach( function(el, ind, array) {
+    result = (el.cond(i)) ? el.label : result;
+  })
+  console.log( result );
 }
+
